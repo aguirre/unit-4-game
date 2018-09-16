61c2161b6e37370ef277a3a2e771af5e0b78b257
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-09-15 20:38:09 -0700

    update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 var fighterSelected = false;
 var enemySelected = false;
+var enemiesDefeated = 0;
 var fighter = {};
 var enemy = {};
-var enemiesDefeated = 0;
 gameOver = false;
 
 var symbiote = {
@@ -33,4 +33,66 @@ var venom = {
   attack: 25
 };
 
-fuction initializeFighter(selectedFighter)
+function initializeFighter(selectedFighter) {
+  fighter.name = selectedFighter.name;
+  fighter.health = selectedFighter.health;
+  fighter.baseAttack = selectedFighter.baseAttack;
+  fighter.attack = selectedFighter.attack;
+}
+
+function initializeEnemy(selectedEnemy) {
+  enemy.name = selectedEnemy.name;
+  enemy.health = selectedEnemy.health;
+  enemy.baseAttack = selectedEnemy.baseAttack;
+  enemy.attack = selectedEnemy.attack;
+}
+
+function moveEnemy() {
+  $(".availableFighter")
+    .removeClass("availableFighter")
+    .addClass("availableEnemies");
+  $("#enemyRow").append($(".availableEnemies"));
+}
+
+function resetGame() {
+  $("#symbioteImage")
+    .children(".health")
+    .html(symbiote.health);
+  $("#spidermanImage")
+    .children(".health")
+    .html(spiderman.health);
+  $("#carnageImage")
+    .children(".health")
+    .html(carnage.health);
+  $("#venomImage")
+    .children(".health")
+    .html(venom.health);
+  $(".character")
+    .removeClass("fighter availableEnemies enemyCharacter")
+    .addClass("availableFighter");
+  var available = $(".availableFighter").show();
+  $("#fighterRow").html(available);
+  $("#gameMessage").empty();
+  $("#fighterRow").show();
+  $("#gameplayRow").hide();
+  $("#messageRow").hide();
+  $("#restartRow").hide();
+  $("#enemyRow").hide();
+  fighterSelected = false;
+  enemySelected = false;
+  enemiesDefeated = 0;
+  gameOver = false;
+  fighter = {};
+  enemy = {};
+}
+
+$(document).ready(function() {
+  $("#symbioteImage").on("click", function() {
+    console.log("Symbiote Suit has been selected");
+    if (fighterSelected == false) {
+      $("#gameMessage").empty();
+      initializeFighter(symbiote);
+      fighterSelected = true;
+    }
+  });
+});
